f76925763be603b1dbf507eeb055fb34d7d75f4f
ns2148634 <ewn.utt@gmail.com>
2025-10-20 23:30:14 +0800

    Update page.tsx

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { useEffect, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+
 const COLORS = [
   "#F5F5F5", "#EAF6FF", "#F9F6EF",
   "#FFD700", "#FDC1C5", "#A6E1FA", "#46C2D6",
