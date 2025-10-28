7f215a495b4000f15160aeef4bed4210313e6dde
ns2148634 <ewn.utt@gmail.com>
2025-10-28 13:25:21 +0800

    更新 showall 百業名片最新功能與UI

--- app/page.tsx
+++ app/page.tsx
@@ -118,7 +118,7 @@ export default function Home() {
           <Link href="/upload" className="block">
             <div className="bg-gradient-to-br from-yellow-400 to-pink-400 rounded-2xl px-0 py-8 shadow-2xl flex flex-col items-center transition-transform hover:scale-105 active:scale-95 cursor-pointer">
               <span className="text-4xl mb-3 drop-shadow">⏫</span>
-              <span className="text-lg font-bold tracking-wide mb-1 text-white">100元上架名片</span>
+              <span className="text-lg font-bold tracking-wide mb-1 text-white">上傳名片</span>
               <span className="text-xs text-pink-100 text-center">新創、商家與個人都能刊登</span>
             </div>
           </Link>
