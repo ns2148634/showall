4f2a708be5e366781c47a5263857b51c47f79690
ns2148634 <ewn.utt@gmail.com>
2025-10-31 08:18:26 +0800

    除錯

--- app/card/[url_slug]/page.tsx
+++ app/card/[url_slug]/page.tsx
@@ -1,7 +1,7 @@
 import CardPage from "./CardPage";
 
 export default function Page({ params }: { params: { url_slug: string } }) {
+  // 在這裡加 console log
   console.log('page.tsx params:', params);
   return <CardPage url_slug={params.url_slug} />;
 }
-
